0873a897dfd24cb9f54c7bd59d35977d1470e398
Shashank AV <shashankav@ShashanAV-iMac.local>
2021-02-21 16:32:43 +0530

    added pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import './App.css';
-import Header from '../src/molecules/header/header';
-import Carosel from '../src/molecules/carosel/carosel.jsx';
-import Card from "./atoms/card/card.jsx";
-import LargeCard from "./atoms/largeCard/largeCard.jsx";
+import Landing from "../src/pages/landing.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Carosel />
-      <Card title="Watch and learn"/>
-      <Card title="Shows you love"/>
-      <Card title="Newly Added"/>
-      <Card title="Discovering Animal Friends"/>
-      <Card title="Superstars"/>
-      <Card title="Investigation Special"/>
-      <Card title="When show biz meets science"/>
-      <LargeCard />
-      <Card title="In first lane"/>
-      <Card title="When disaster strikes"/>
-      <Card title="Bear Grills"/>
-      <Card title="Animals Appeal"/>
-      <Card title="Favouraties"/>
-      <Card title="Need some Space?"/>
+      <Landing />
     </div>
   );
 }
